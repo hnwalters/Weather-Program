21e1afae25cee5a8b19a32e70f7f6d8fcd773d6c
Hayden N. Walters <31044184+hnwalters@users.noreply.github.com>
2017-08-21 12:17:21 -0500

    Add files via upload

--- WeatherReport.java
+++ WeatherReport.java
@@ -1,11 +1,40 @@
 import java.util.Scanner;
-
+	
 public class WeatherReport
 {
-
+	
 	public static void main(String[] args) 
-	{
+		{		
+		System.out.println("Hello, please type to the tempature in degrees Fahrenheit.");
 		
-	}
-
-}
+		Scanner cin = new Scanner(System.in);
+		
+		
+		double temp = cin.nextInt();
+		double c = 5*(temp-32)/9;
+		double answer =  c;
+		
+		
+		System.out.println(answer);
+		
+		
+		 if(temp<=32)
+		 {
+			 System.out.println("It is freezing.");
+		 }
+	
+		  if(temp>=90)
+		 {
+			 System.out.println("It is hot.");
+		 }
+		   if(temp>=212)
+		  {
+			  System.out.println("Okay, so yeah you are probably gonna die!");
+		  }
+		 
+		 
+	cin.close();
+	
+	
+		}}
+	
