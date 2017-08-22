a99a6179dc2a18db00074cc96c39e0ccd34d250c
Hayden N. Walters <31044184+hnwalters@users.noreply.github.com>
2017-08-22 11:42:13 -0500

    Add files via upload

--- WeatherReport.java
+++ WeatherReport.java
@@ -1,3 +1,4 @@
+//Hayden N. Walters
 import java.util.Scanner;
 	
 public class WeatherReport
@@ -5,9 +6,9 @@ public class WeatherReport
 	
 	public static void main(String[] args) 
 		{		
-		System.out.println("Hello, please type to the tempature in degrees Fahrenheit.");
+		System.out.println("Hello, please type to the tempature in degrees Fahrenheit."); //States the objective
 		
-		Scanner cin = new Scanner(System.in);
+		Scanner cin = new Scanner(System.in); //To provide user input
 		
 		
 		double temp = cin.nextInt();
@@ -15,19 +16,19 @@ public class WeatherReport
 		double answer =  c;
 		
 		
-		System.out.println(answer);
+		System.out.println(answer); //displays the outcome
 		
 		
 		 if(temp<=32)
 		 {
-			 System.out.println("It is freezing.");
+			 System.out.println("It is freezing."); // if 32 degrees or lower states it is freezing
 		 }
 	
 		  if(temp>=90)
 		 {
-			 System.out.println("It is hot.");
+			 System.out.println("It is hot."); // if 90 degrees or higher says it is hot
 		 }
-		   if(temp>=212)
+		   if(temp>=212) // if 212 degrees or higher says okay, so yeah you are probably gonna die!
 		  {
 			  System.out.println("Okay, so yeah you are probably gonna die!");
 		  }
